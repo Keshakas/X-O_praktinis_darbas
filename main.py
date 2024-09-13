ead000a23fa7ef23e2cf1f5337bd585ef191d3e8
Keshakas <kestutis.kazakevicius@gmail.com>
2024-09-13 12:24:31 +0300

    Pridetas veiksmu pasirinkimas ir rezultato skaiciavimas

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import random
 
 lentele = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
+rezultatas_x = 0
+rezultatas_o = 0
 
 def spausdinti_lentele():
-    print("\n|", lentele[0], "|", lentele[1], "|", lentele[2], "|")
+    print("\n|", lentele[6], "|", lentele[7], "|", lentele[8], "|")
     print("|", lentele[3], "|", lentele[4], "|", lentele[5], "|")
-    print("|", lentele[6], "|", lentele[7], "|", lentele[8], "|")
+    print("|", lentele[0], "|", lentele[1], "|", lentele[2], "|")
 
 
 def ivestis():
     while True:
-        pasirinkimas = input("Pasirinkite skaičių (1-9): ")
+        pasirinkimas = input("Pasirinkite skaičių: ")
         if pasirinkimas.isdigit() and 1 <= int(pasirinkimas) <= 9:
             if lentele[int(pasirinkimas) - 1] not in ["X", "O"]:
                 return int(pasirinkimas)
@@ -40,6 +41,7 @@ def zaidejas_o():
 
 
 def tikrinimas_x():
+    global rezultatas_x, lentele
     if (((((((lentele[0] == lentele[1] == lentele[2] == "X" or
             lentele[3] == lentele[4] == lentele[5] == "X") or
             lentele[6] == lentele[7] == lentele[8] == "X") or
@@ -49,8 +51,9 @@ def tikrinimas_x():
             lentele[0] == lentele[4] == lentele[8] == "X") or
             lentele[2] == lentele[4] == lentele[6] == "X"):
         print("\n-- Laimėjo -- X -- žaidėjas! --")
+        rezultatas_x += 1
+        lentele = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     else:
-        lygiuju_tikrinimas(lentele)
         if lygiuju_tikrinimas(lentele):
             print("\n-- Žaidimas baigėsi lygiosiomis! --")
         else:
@@ -58,6 +61,7 @@ def tikrinimas_x():
 
 
 def tikrinimas_o():
+    global rezultatas_o, lentele
     if (((((((lentele[0] == lentele[1] == lentele[2] == "O" or
             lentele[3] == lentele[4] == lentele[5] == "O") or
             lentele[6] == lentele[7] == lentele[8] == "O") or
@@ -67,9 +71,9 @@ def tikrinimas_o():
             lentele[0] == lentele[4] == lentele[8] == "O") or
             lentele[2] == lentele[4] == lentele[6] == "O"):
         print("\n-- Laimėjo -- O -- žaidėjas! --")
-
+        rezultatas_o += 1
+        lentele = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     else:
-        lygiuju_tikrinimas(lentele)
         if lygiuju_tikrinimas(lentele):
             print("\n-- Žaidimas baigėsi lygiosiomis! --")
         else:
@@ -83,6 +87,10 @@ def lygiuju_tikrinimas(lentele):
     return True
 
 
+def rezultatas():
+    print(f"REZULTATAS: \nŽaidėjas X --> {rezultatas_x} \nŽaidėjas O --> {rezultatas_o}")
+
+
 def zaidimo_pradzia():
     num = random.randint(1, 2)
     if num == 1:
@@ -92,5 +100,17 @@ def zaidimo_pradzia():
 
 
 print("\n-- Kryžiukų - nuliukų žaidimas --")
-spausdinti_lentele()
-zaidimo_pradzia()
+while True:
+    try:
+        pasirinkimas = int(input("\n1 - Žaisti \n2 - Rezultatas \n3 - Išeiti \n"))
+        match pasirinkimas:
+            case 1:
+                print("Pirmas žaidėjas parenkamas \natsitiktine tvarka")
+                spausdinti_lentele()
+                zaidimo_pradzia()
+            case 2:
+                rezultatas()
+            case 3:
+                break
+    except ValueError:
+        print("Pasirinkte skaičių 1-3)")
