13cadd8c8510477792194568e78f06ee05283a47
Keshakas <kestutis.kazakevicius@gmail.com>
2024-09-13 15:31:36 +0300

    Pridetas zaidimas su kompiuteriu

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import random
 lentele = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 rezultatas_x = 0
 rezultatas_o = 0
+rezultatas_k = 0
+komp_zaidimas = False
 
 def spausdinti_lentele():
     print("\n|", lentele[6], "|", lentele[7], "|", lentele[8], "|")
@@ -20,6 +22,13 @@ def ivestis():
                 print("Klaida: Ši vieta jau užimta. Bandykite kitą skaičių.")
         else:
             print("Klaida: netinkamas simbolis")
+
+
+def kompiuterio_ivestis():
+    while True:
+        pasirinkimas = random.randint(1, 9)
+        if lentele[int(pasirinkimas) - 1] not in ["X", "O"]:
+            return int(pasirinkimas)
 
 
 def zaidejas_x():
@@ -40,8 +49,18 @@ def zaidejas_o():
     tikrinimas_o()
 
 
+def komoiuteris_o():
+    print("\nŽaidėjas -----O-----")
+    pasirinkimas = kompiuterio_ivestis()
+    print(f"Kompiuteris pasirinko {pasirinkimas}")
+    lentele[pasirinkimas - 1] = "O"
+
+    spausdinti_lentele()
+    tikrinimas_o()
+
+
 def tikrinimas_x():
-    global rezultatas_x, lentele
+    global rezultatas_x, lentele, komp_zaidimas
     if (((((((lentele[0] == lentele[1] == lentele[2] == "X" or
             lentele[3] == lentele[4] == lentele[5] == "X") or
             lentele[6] == lentele[7] == lentele[8] == "X") or
@@ -53,15 +72,20 @@ def tikrinimas_x():
         print("\n-- Laimėjo -- X -- žaidėjas! --")
         rezultatas_x += 1
         lentele = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        komp_zaidimas = False
     else:
         if lygiuju_tikrinimas(lentele):
             print("\n-- Žaidimas baigėsi lygiosiomis! --")
+            lentele = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         else:
-            zaidejas_o()
+            if komp_zaidimas:
+                komoiuteris_o()
+            else:
+                zaidejas_o()
 
 
 def tikrinimas_o():
-    global rezultatas_o, lentele
+    global rezultatas_o, lentele, rezultatas_k, komp_zaidimas
     if (((((((lentele[0] == lentele[1] == lentele[2] == "O" or
             lentele[3] == lentele[4] == lentele[5] == "O") or
             lentele[6] == lentele[7] == lentele[8] == "O") or
@@ -71,11 +95,16 @@ def tikrinimas_o():
             lentele[0] == lentele[4] == lentele[8] == "O") or
             lentele[2] == lentele[4] == lentele[6] == "O"):
         print("\n-- Laimėjo -- O -- žaidėjas! --")
-        rezultatas_o += 1
         lentele = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        if komp_zaidimas:
+            rezultatas_k += 1
+        else:
+            rezultatas_o += 1
+        komp_zaidimas = False
     else:
         if lygiuju_tikrinimas(lentele):
             print("\n-- Žaidimas baigėsi lygiosiomis! --")
+            lentele = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         else:
             zaidejas_x()
 
@@ -88,7 +117,7 @@ def lygiuju_tikrinimas(lentele):
 
 
 def rezultatas():
-    print(f"REZULTATAS: \nŽaidėjas X --> {rezultatas_x} \nŽaidėjas O --> {rezultatas_o}")
+    print(f"REZULTATAS:\nŽaidėjas X  --> {rezultatas_x}\nŽaidėjas O  --> {rezultatas_o}\nKompiuteris --> {rezultatas_k}")
 
 
 def zaidimo_pradzia():
@@ -99,18 +128,23 @@ def zaidimo_pradzia():
         zaidejas_o()
 
 
-print("\n-- Kryžiukų - nuliukų žaidimas --")
+print("\n-- KRYŽIUKŲ - NULIUKŲ ŽAIDIMAS --")
 while True:
     try:
-        pasirinkimas = int(input("\n1 - Žaisti \n2 - Rezultatas \n3 - Išeiti \n"))
+        pasirinkimas = int(input("\n1 - Žaisti dviems  \n2 - Žaisti su kompiuteriu \n3 - Rezultatas \n4 - Išeiti \n"))
         match pasirinkimas:
             case 1:
-                print("Pirmas žaidėjas parenkamas \natsitiktine tvarka")
+                print("Pirmasis žaidimo ėjimas \nparenkamas atsitiktine tvarka")
                 spausdinti_lentele()
                 zaidimo_pradzia()
             case 2:
-                rezultatas()
+                print("Kompiuteriui priskirtas \n-----O----- žaidėjas")
+                komp_zaidimas = True
+                spausdinti_lentele()
+                zaidejas_x()
             case 3:
+                rezultatas()
+            case 4:
                 break
     except ValueError:
         print("Pasirinkte skaičių 1-3)")
